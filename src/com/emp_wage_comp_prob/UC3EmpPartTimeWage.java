2b7995d4e7a5598d7f6b613b5969a57534189fba
Rampravesh25 <rampraveshmallah@gmail.com>
2022-05-22 21:04:34 +0530

    Third Commit

---
+++ src/com/emp_wage_comp_prob/UC3EmpPartTimeWage.java
@@ -0,0 +1,23 @@
+package com.emp_wage_comp_prob;
+
+public class UC3EmpPartTimeWage {
+    public static void main(String[] args) {
+        int partTime = 1;
+        int fullTime = 0;
+        int perHoursWage = 20;
+
+        int empType = (int) (Math.random() * 100) % 2;
+
+        if (empType == partTime) {
+            System.out.println(partTime * perHoursWage + "Rs");
+        } else {
+            System.out.println("Not Calculated");
+        }
+        if (empType == 12) {
+            System.out.println(fullTime * perHoursWage + "Rs");
+        } else {
+            System.out.println("Not Calculated");
+        }
+
+    }
+}
--- src/com/emp_wage_comp_prob/UC3_Emp_PartTime_Wage.java
+++
@@ -1,7 +0,0 @@
-package com.emp_wage_comp_prob;
-
-public class UC3_Emp_PartTime_Wage {
-    public static void main(String[] args) {
-
-    }
-}
